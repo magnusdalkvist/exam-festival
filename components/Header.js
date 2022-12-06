9d1f08224e9f3edebad9b959ecedc3d6a8a02b5a
Magnus Dalkvist <magn552n@stud.kea.dk>
2022-12-06 12:18:35 +0100

    burger menu added

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import Anchor from "./Anchor";
 
 function Header() {
+  const [state, setState] = useState("closed");
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Anchor className="logo" href="/">
-            Greenmark
-          </Anchor>
-        </li>
-      </ul>
-      <button>Burger</button>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Anchor className="logo" href="/">
+              Greenmark
+            </Anchor>
+          </li>
+        </ul>
+        <button onClick={() => setState("open")}>Burger</button>
+      </nav>
+      <div className={"menu " + state}>
+        <button onClick={() => setState("closed")}>Burger</button>
+        <ul>
+          <Anchor href="/">Home</Anchor>
+          <Anchor href="/booking">Booking</Anchor>
+          <Anchor href="/schedule">Schedule</Anchor>
+        </ul>
+      </div>
+    </header>
   );
 }
 
--- components/Layout.js
+++ components/Layout.js
@@ -4,15 +4,9 @@ import Header from "./Header";
 export default function Layout({ children, navData }) {
   return (
     <>
-      <header>
-        <Header></Header>
-      </header>
-
+      <Header />
       <main>{children}</main>
-
-      <footer>
-        <Footer></Footer>
-      </footer>
+      <Footer />
     </>
   );
 }
