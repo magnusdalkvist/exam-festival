d7e36907f107b3d372ccff25f457905435665218
Magnus Dalkvist <magn552n@stud.kea.dk>
2022-12-13 12:18:44 +0100

    form validation

--- components/BookingForm.jsx
+++ components/BookingForm.jsx
@@ -12,6 +12,7 @@ function BookingForm(props) {
   const [res, setRes] = useState();
   const [cart, setCart] = useState(fee);
   const [info, setInfo] = useState([]);
+  const [val, setVal] = useState(false);
 
   const reset = () => {
     setInfo([]);
@@ -31,9 +32,7 @@ function BookingForm(props) {
   };
 
   const reserveSpot = async (e) => {
-    if (cart.findIndex((e) => e.type == "ticket" && e.amount > 0) > -1) {
-      e.preventDefault();
-
+    if (cart.findIndex((e) => e.type == "ticket" && e.amount > 0) > -1 && val) {
       const options = {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
@@ -50,17 +49,29 @@ function BookingForm(props) {
   };
 
   const getInfo = () => {
-    document.querySelectorAll("form#test_form").forEach((e) => {
+    document.querySelectorAll("#test_form[data-type='regular']").forEach((e) => {
       const person = {};
       person.firstname = e.firstname.value;
       person.lastname = e.lastname.value;
       person.email = e.email.value;
       person.phone = e.phone.value;
+      person.type = "regular";
+      setInfo((info) => info.concat(person));
+    });
+    document.querySelectorAll("#test_form[data-type='vip']").forEach((e) => {
+      const person = {};
+      person.firstname = e.firstname.value;
+      person.lastname = e.lastname.value;
+      person.email = e.email.value;
+      person.phone = e.phone.value;
+      person.type = "vip";
       setInfo((info) => info.concat(person));
     });
   };
 
   const completeOrder = () => {
+    console.log(info);
+    let status;
     const options = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -69,8 +80,37 @@ function BookingForm(props) {
 
     fetch("http://localhost:8080/fullfill-reservation", options)
       .then((response) => response.json())
-      .then((response) => console.log(response))
+      .then((response) => sendData(response))
       .catch((err) => console.error(err));
+
+    const sendData = (response) => {
+      info.forEach((order) => {
+        if (response.status != 500) {
+          const options = {
+            method: "POST",
+            headers: {
+              apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhcmRva2RjeWVpeGptaGhteGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAxNzE5OTYsImV4cCI6MTk4NTc0Nzk5Nn0.uig3ma3vMK5hoixd-GrRayxapGxXaEFO1UBYDS_RZdw",
+              Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhcmRva2RjeWVpeGptaGhteGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAxNzE5OTYsImV4cCI6MTk4NTc0Nzk5Nn0.uig3ma3vMK5hoixd-GrRayxapGxXaEFO1UBYDS_RZdw",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              order_id: res.id,
+              spot: area,
+              ticket: order.type,
+              info: order,
+              green: true,
+              tent2: 1,
+              tent3: 2,
+            }),
+          };
+
+          fetch("https://iardokdcyeixjmhhmxeu.supabase.co/rest/v1/booking", options)
+            .then((response) => response.json())
+            .then((response) => console.log(response))
+            .catch((err) => console.error(err));
+        }
+      });
+    };
   };
 
   return (
@@ -84,7 +124,7 @@ function BookingForm(props) {
             <TicketForm cart={cart} setCart={setCart} />
           </Collapse>
           <Collapse disabled={cart.findIndex((e) => e.type == "ticket" && e.amount > 0) < 0} title="Ticket info">
-            <InfoForm cart={cart} />
+            <InfoForm cart={cart} validation={(res) => setVal(res)} />
           </Collapse>
         </Collapse.Group>
       )}
--- components/forms/InfoForm.jsx
+++ components/forms/InfoForm.jsx
@@ -1,7 +1,14 @@
 import styles from "../../styles/Booking.module.css";
+import React, { useRef } from "react";
 import { Input } from "@nextui-org/react";
 
 function InfoForm(props) {
+  const theForm = useRef();
+
+  const validate = () => {
+    console.log(theForm.current.checkValidity());
+  };
+
   return (
     <div className={styles.info_list}>
       {props.cart.map((item, index) => {
@@ -10,18 +17,18 @@ function InfoForm(props) {
 
           for (let i = 0; i < item.amount; i++) {
             fields.push(
-              <form id="test_form" className={styles.info} key={i}>
+              <form ref={theForm} id="test_form" data-type={item.value} className={styles.info} key={i} onChange={validate}>
                 <b>Ticket {i + 1}</b>
-                <Input width="100%" name="firstname" label="Firstname" placeholder="John" required clearable key={1 * (i * 4 + 1)}></Input>
-                <Input width="100%" name="lastname" label="Lastname" placeholder="Doe" required clearable key={2 * (i * 5 + 1)}></Input>
-                <Input width="100%" name="email" label="Email" placeholder="johndoe@example.com" required clearable key={3 * (i * 4 + 1)}></Input>
-                <Input width="100%" name="phone" label="Phone" placeholder="12345678" required clearable key={4 * (i * 4 + 1)}></Input>
+                <Input width="100%" name="firstname" label="Firstname" placeholder="John" required pattern="[A-Za-z]{2,}" key={1 * (i * 4 + 1)}></Input>
+                <Input width="100%" name="lastname" label="Lastname" placeholder="Doe" required pattern="[A-Za-z]{2,}" key={2 * (i * 5 + 1)}></Input>
+                <Input width="100%" name="email" label="Email" placeholder="johndoe@example.com" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" key={3 * (i * 4 + 1)}></Input>
+                <Input width="100%" name="phone" label="Phone" placeholder="12345678" required pattern="[+0-9]{8,}" key={4 * (i * 4 + 1)}></Input>
               </form>
             );
           }
           return (
-            <div key={index}>
-              <h1 className={styles.info_type}>{item.name}</h1>
+            <div className={styles.info_item} key={index}>
+              <h1 className={styles.info_type}>{item.value}</h1>
               {fields}
             </div>
           );
