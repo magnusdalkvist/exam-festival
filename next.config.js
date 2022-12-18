f60e178659c5bb7bc81f6a97dbfa930756c4c348
Magnus Dalkvist <magn552n@stud.kea.dk>
2022-12-18 19:57:35 +0100

    added domain to images

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    domains: ["localhost", "placeimg.com"],
+    domains: ["localhost", "placeimg.com", "greenmark.fly.dev"],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [400, 500, 600, 700],
   },
