2804139fc6387d82080370c489a08eeb2fc44090
Magnus Dalkvist <magn552n@stud.kea.dk>
2022-12-18 13:44:27 +0100

    ssrprovider issue fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,7 +7,7 @@ import Head from "next/head";
 function MyApp({ Component, pageProps, bandData }) {
   const [state, setState] = useState("close");
   return (
-    <>
+    <SSRProvider>
       <Head>
         <title>GREENMARK FESTIVAL</title>
         <meta name="description" content="GREENMARK FESTIVAL" />
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps, bandData }) {
       <Layout state={state} setState={(state) => setState(state)}>
         <Component {...pageProps} bandData={bandData} setState={(state) => setState(state)} />
       </Layout>
-    </>
+    </SSRProvider>
   );
 }
 
@@ -25,7 +25,7 @@ export default MyApp;
 MyApp.getInitialProps = async (appContext) => {
   // Provide the appContext, in order to do 404's
   const appProps = await App.getInitialProps(appContext);
-  const band = await fetch("https://greenmark.fly.dev/bands/");
+  const band = await fetch("http://localhost:8080/bands/");
   const bandData = await band.json();
   return { ...appProps, bandData };
 };
